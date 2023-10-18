1d3fe1a2ae9bbc30506312d25e31e2639bb8a1b4
Krystian Maccs <krystianmaccs@gmail.com>
2023-10-18 21:09:19 +0100

    feat: defined superuser method for user manager

--- accounts/managers.py
+++ accounts/managers.py
@@ -44,6 +44,32 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
+    def create_superuser(self, username, first_name, last_name, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superusers must have is_staff=True"))
+        
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superusers must have is_suoeruser=True"))
+        
+        if not password:
+            raise ValueError(_("Superusers must have a password"))
+        
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(_("Admin account: An email is required"))
+        
+        user = self.create_user(
+            username, first_name, last_name, email, password, **extra_field
+        )
+        user.save(using=self._db)
+        return user
+        
     
             
             
--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,7 @@ from .managers import CustomUserManager
 
 
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     pkid = models.BigAutoField(primary_key=True, editable=False)
     id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
